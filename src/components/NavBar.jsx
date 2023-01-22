67e858577abe9733537228d2039067439fd74542
mhamed <mohamedhame2ed@gmail.com>
2023-01-22 02:39:44 +0200

    add run build

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-          <a href="/" class="flex items-center">
+          <Link to="/" class="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               class="mr-3 h-6 sm:h-9"
@@ -32,7 +32,7 @@ const NavBar = () => {
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               STORE
             </span>
-          </a>
+          </Link>
           <div class="flex items-center">
             {currentUser ? (
               currentUser
